ff06e8048f8427bdc0746b102cf2afa4f7155204
August Matthew Quinlan <yoshicat336@gmail.com>
2022-03-18 12:21:19 -0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,7 @@
 enum RadioMessage {
     message1 = 49434,
-    Hello = 49337
+    Hello = 49337,
+    Help = 43444
 }
 input.onButtonPressed(Button.A, function () {
     radio.sendMessage(RadioMessage.Hello)
@@ -9,5 +10,5 @@ input.onButtonPressed(Button.AB, function () {
     radio.sendMessage(RadioMessage.message1)
 })
 input.onButtonPressed(Button.B, function () {
-    radio.sendMessage(RadioMessage.message1)
+    radio.sendMessage(RadioMessage.Help)
 })
